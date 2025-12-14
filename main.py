ebeabf7196415205dc15b280630c7c5bc917677f
Matthew <motbro007@mail.com>
2025-12-14 23:29:33 +0400

    fuck this

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 def first_func(first, second):
-    return first+second
+    return first + second
+
 
 def second_func(first):
-    return first+420
+    return first + 420
 
+
+print("пре комит этот ваш чёрт бы его побрал")
+print("я ваши рты ебал")
